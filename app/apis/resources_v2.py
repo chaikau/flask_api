2fb4024d9a10ed93155d23c1cef9793c2daeff69
Chaikau Skin <chaikau@users.noreply.github.com>
2018-05-02 10:27:25 +0800

    Update resources_v2.py

--- app/apis/resources_v2.py
+++ app/apis/resources_v2.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-    @version:  python 3.6
-    @FileName: auth.py
-    @Author:   chaikau
-    @Time：    2018-03-24 19:39
-    @Description: resource api with swagger
-"""
+
 from lib.Logging import Logging
 from flask import Blueprint, g, request
 from flask_restplus import Resource, fields, reqparse, Api
